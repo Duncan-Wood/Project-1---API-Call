5b87ffe1ca2cac5ad0836e2057abfd724578feca
Duncan <duncanwoodpro@gmail.com>
2023-02-04 14:33:00 -0500

    I added several things here. Probably should have done a few more commits. Spent a long time researching async function and implemented the await operator. Messed around with the Google Maps API to try and add more flair to my design with a picture from the city that was searched.

--- README.md
+++ README.md
@@ -3,20 +3,20 @@ For project 1, I will use the weather API from [weather API](https://www.weather
 
 This API is very powerful because it contains up-to-date information on weather around the world from temperature to wind speed. I plan on using the various pieces of data to create a dynamic result depending on if it is rainy, sunny, or cloudy. My site will also incorperate suggestions for clothing or accessories. For example, if it is raining, the text at the bottom will suggest the user to bring an umbrella. I will create a Trello board to set and reach daily goals and continue innovating the design and functionality of my site. 
 
-Here is a general layout of what my site will look like, although this is only my first draft. 
+Find a general layout of what my site will look like on Figma, although this is only my first draft. 
 
-![figma whiteboard](https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2Flqq53UhGYyQJDjdMGqSdsw%2FWeather-API-call%3Fnode-id%3D0%253A1%26t%3DzXTFw0qBEkRlrPsw-1)
 
 ##MVP 
-By the End of today (2/3)
-- [ ] Create a Trello board planning out the rest of project week so that by the end of my work I will have 20 meaningful git commits
-- [ ] fully explore the [weather API](https://www.weatherapi.com) more in depth to get a better sense of all of the data I have to work with. For example, I want to find all the possible conditions of weather (clear, cloudy, partially cloudy, etc.) so that I can create a polymorphous design. 
-- [ ] Dedicate time to psuedocode for my HTML, CSS, and Javascript
+- Create a Trello board planning out the rest of project week so that by the end of my work I will have 20 meaningful git commits
+- fully explore the [weather API](https://www.weatherapi.com) more in depth to get a better sense of all of the data I have to work with. For example, I want to find all the possible conditions of weather (clear, cloudy, partially cloudy, etc.) so that I can create a polymorphous design. 
+- Dedicate time to psuedocode for my HTML, CSS, and Javascript
     - Figure out what global variables I need
     - Plan out what functions I need 
     - Push myself with CSS and plan out an elegant design
+- add a catch method into my async function in order to prevent my code from breaking if unable to fulfill a promise. 
 
 
 ##Stretch Goals
-- [ ] A weather API call is probably one of the most common projects of all beginner coders. Think about what I can do to make mine stand out!
-- [ ] Add an inspirational quote of the day at the bottom of my weather API
+- Correctly utilize the await operator instead of .then() in order to create a more elegant and readable design for my code. 
+- A weather API call is probably one of the most common projects of all beginner coders. Think about what I can do to make mine stand out!
+- Add an inspirational quote of the day at the bottom of my weather API
--- script.js
+++ script.js
@@ -1,25 +1,48 @@
+//Sources
+// ChatGPT used for rubberducking and adapted to suit my needs
+// Why I used await instead of .then (https://dev.to/masteringjs/using-then-vs-async-await-in-javascript-2pma)
+// await operator to clean up async function (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/await)
 
-let button = document.querySelector("#searchButton")
+//API Keys
+//Weather API Key: 933bde006f044a14a6515410230302
+//Google Maps API Key: AIzaSyAgSgkNWoOIca1vh3nUjZEHq9hOkiwwWio
 
-async function getData (event) {
-    event.preventDefault()
-    let textInput = document.querySelector("#inputBar").value
 
-    const url = `http://api.weatherapi.com/v1/current.json?key=933bde006f044a14a6515410230302&q=${textInput}`
+//Global Variables
+const searchBtn = document.querySelector("#searchBtn");
+const cityInput = document.querySelector("#cityInput");
+const cityName = document.querySelector("#cityName");
+const region = document.querySelector("#region");
+const time = document.querySelector("#time");
+const weather = document.querySelector("#weather");
+const weatherIcon = document.querySelector("#weatherIcon");
+//having a hard time getting google image to populate. Look for alternatives or ask for help. 
+const image = document.querySelector("#image");
 
-    fetch(url)
-        .then(res => {
-        return res.json()
-        })
-        .then (res => {
-            console.log(url)
-            console.log(`Success!`, res)
 
-        })
-        .catch(err => {
-            console.log("Error!", err)
-        })
-}
+//Functions
 
-//Add event listener for the button to call function when pressed
-button.onclick = getData
+//nested async function within event listener and used arrow function
+searchBtn.addEventListener("click", async () => {
+  const city = cityInput.value;
+  const weatherApiUrl = `https://api.weatherapi.com/v1/current.json?key=933bde006f044a14a6515410230302&q=${city}`;
+
+  //await is stronger than .then because await pauses the function until the promise settles. This cuts down on the amount of code I need to write.
+  const weatherResponse = await fetch(weatherApiUrl);
+  const weatherData = await weatherResponse.json();
+  console.log(weatherData)
+  
+  cityName.innerHTML = `City: ${weatherData.location.name}`;
+  region.innerHTML = `Region: ${weatherData.location.region}`;
+  time.innerHTML = `Time: ${weatherData.location.localtime}`;
+  weather.innerHTML = `Weather: ${weatherData.current.condition.text}`;
+  weatherIcon.src = weatherData.current.condition.icon;
+
+  //currently struggling to get the Google Maps API to include the specified pictures
+  const googleApiUrl = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${city}&key=AIzaSyAgSgkNWoOIca1vh3nUjZEHq9hOkiwwWio`;
+  const googleResponse = await fetch(googleApiUrl);
+  const googleData = await googleResponse.json();
+  console.log(googleData)
+
+  image.src = googleData.results[0].icon;
+  })
